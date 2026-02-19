be97129455a31c2667314205d6ce16266322d43a
SAY007-DEV <mail.to.sayooj.kp@gmail.com>
2026-02-19 16:04:33 +0530

    login route aded

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,18 +1,54 @@
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 
+let users = [];
 
-export async function  POST(req){
-    try {
-        const { email ,password}= await req.json();
+export async function POST(req) {
+  try {
+    const { email, password } = await req.json();
 
-        if (!email || !password){
-            return NextResponse.json({ message : "userName and password are required"},
-                {status:400}
-            );
-        }
-
-    } catch (error) {
-        return NextResponse.json({message:error.message},{status:500})
+    if (!email || !password) {
+      return NextResponse.json(
+        { message: "All fields are required" },
+        { status: 400 }
+      );
     }
+
+    const user = users.find((user) => user.email === email);
+
+    if (!user) {
+      return NextResponse.json(
+        { message: "Invalid credentials" },
+        { status: 401 }
+      );
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return NextResponse.json(
+        { message: "Invalid credentials" },
+        { status: 401 }
+      );
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    return NextResponse.json(
+      { message: "Login successful", token },
+      { status: 200 }
+    );
+
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Server error", error: error.message },
+      { status: 500 }
+    );
+  }
 }
